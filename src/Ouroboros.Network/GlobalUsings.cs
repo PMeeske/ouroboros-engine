00b107073a458dfd01ecf38775951fb656664879
meeske <philip.meeske@adesso.de>
2026-02-11 13:14:58 +0100

    Add comprehensive System global usings to all engine projects

--- src/Ouroboros.Network/GlobalUsings.cs
+++ src/Ouroboros.Network/GlobalUsings.cs
@@ -1,13 +1,20 @@
 // <copyright file="GlobalUsings.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
-
 global using System;
+global using System.Collections.Concurrent;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
+global using System.Diagnostics;
+global using System.IO;
 global using System.Linq;
+global using System.Net.Http;
+global using System.Reflection;
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
+global using System.Text.RegularExpressions;
+global using System.Threading;
 global using System.Threading.Tasks;
 global using Ouroboros.Core.Monads;
 global using Ouroboros.Domain.States;
